0212b7efbb3219260de91b8b1b68fc168a0ac655
Mohdsaad83 <mfm12208@gmail.com>
2025-06-03 13:39:54 +0300

    frontend

--- src/components/CarList.jsx
+++ src/components/CarList.jsx
@@ -1,8 +1,10 @@
 import React from "react"
 import CarCard from "./CarCard"
+import Navbar from "./Navbar"
 
 const CarList = ({ cars }) => (
   <div>
+    <Navbar />
     <h2 style={{ textAlign: "center", color: "#1a237e" }}>Car List</h2>
     {cars && cars.length > 0 ? (
       cars.map((car, idx) => <CarCard car={car} key={idx} />)
---
+++ src/components/Navebar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Navbar = () => (
+  <nav className="navbar">
+    <div className="navbar-title">
+      Majlis Motors
+    </div>
+  </nav>
+)
+
+export default Navbar
